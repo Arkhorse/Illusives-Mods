8ffe50f212486657c725384da92c82b303cacbc6
Arkhorse <20694721+Arkhorse@users.noreply.github.com>
2020-12-22 16:00:57 -0400

    files are now taken from the WoT_GUI_Tracking folder

--- Single Nation MOE/unpack.bat
+++ Single Nation MOE/unpack.bat
@@ -1,19 +1,19 @@
 @title "MOE Nation maker by The Illusion."
 
 @Echo off
-@set "filesLocation=Source\GUI\gui\maps\vehicles\decals\insignia"
+@set "mainFilesLocation=..\WoT_GUI_Tracking\Source\gui\maps\vehicles\decals"
 
-start cmd /k CALL Builder.bat china, TheIllusion.SingleNationMOE.China.wotmod, Source\GUI\gui\maps\vehicles\decals\insignia
-start cmd /k CALL Builder.bat chuck, TheIllusion.SingleNationMOE.Chuck.wotmod, Source\GUI\gui\maps\vehicles\decals\insignia\custom
-start cmd /k CALL Builder.bat czech, TheIllusion.SingleNationMOE.Czech.wotmod, Source\GUI\gui\maps\vehicles\decals\insignia
-start cmd /k CALL Builder.bat france_uk, TheIllusion.SingleNationMOE.FranceUK.wotmod, Source\GUI\gui\maps\vehicles\decals\insignia
-start cmd /k CALL Builder.bat germany, TheIllusion.SingleNationMOE.Germany.wotmod, Source\GUI\gui\maps\vehicles\decals\insignia
-start cmd /k CALL Builder.bat italian, TheIllusion.SingleNationMOE.Italy.wotmod, Source\GUI\gui\maps\vehicles\decals\insignia
-start cmd /k CALL Builder.bat japan, TheIllusion.SingleNationMOE.Japan.wotmod, Source\GUI\gui\maps\vehicles\decals\insignia
-start cmd /k CALL Builder.bat poland, TheIllusion.SingleNationMOE.Poland.wotmod, Source\GUI\gui\maps\vehicles\decals\insignia
-start cmd /k CALL Builder.bat sweden, TheIllusion.SingleNationMOE.Sweden.wotmod, Source\GUI\gui\maps\vehicles\decals\insignia
-start cmd /k CALL Builder.bat usa, TheIllusion.SingleNationMOE.USA.wotmod, Source\GUI\gui\maps\vehicles\decals\insignia
-start cmd /k CALL Builder.bat ussr, TheIllusion.SingleNationMOE.USSR.wotmod, Source\GUI\gui\maps\vehicles\decals\insignia
-start cmd /k CALL Builder.bat wh, TheIllusion.SingleNationMOE.KV-2R.wotmod, Source\GUI\gui\maps\vehicles\decals\insignia\custom
+start cmd /k CALL Builder.bat china, TheIllusion.SingleNationMOE.China.wotmod, %mainFilesLocation%
+start cmd /k CALL Builder.bat chuck, TheIllusion.SingleNationMOE.Chuck.wotmod, %mainFilesLocation%\custom
+start cmd /k CALL Builder.bat czech, TheIllusion.SingleNationMOE.Czech.wotmod, %mainFilesLocation%
+start cmd /k CALL Builder.bat france_uk, TheIllusion.SingleNationMOE.FranceUK.wotmod, %mainFilesLocation%
+start cmd /k CALL Builder.bat germany, TheIllusion.SingleNationMOE.Germany.wotmod, %mainFilesLocation%
+start cmd /k CALL Builder.bat italian, TheIllusion.SingleNationMOE.Italy.wotmod, %mainFilesLocation%
+start cmd /k CALL Builder.bat japan, TheIllusion.SingleNationMOE.Japan.wotmod, %mainFilesLocation%
+start cmd /k CALL Builder.bat poland, TheIllusion.SingleNationMOE.Poland.wotmod, %mainFilesLocation%
+start cmd /k CALL Builder.bat sweden, TheIllusion.SingleNationMOE.Sweden.wotmod, %mainFilesLocation%
+start cmd /k CALL Builder.bat usa, TheIllusion.SingleNationMOE.USA.wotmod, %mainFilesLocation%
+start cmd /k CALL Builder.bat ussr, TheIllusion.SingleNationMOE.USSR.wotmod, %mainFilesLocation%
+start cmd /k CALL Builder.bat wh, TheIllusion.SingleNationMOE.KV-2R.wotmod, %mainFilesLocation%\custom
 
 pause
